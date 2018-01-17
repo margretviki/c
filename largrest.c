7e61aa61b6c83fbd3fb0cdc7472a06274b2b8444
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-17 06:29:16 -0800

    Create largrest.c

---
+++ largrest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+printf("enter the 3 values"):
+scanf("%f%f,"&a,&b,&c);
+printf("\n LARGEST VALUE IS");
+if(a>b)
+{
+of(a>c)
+printf("%f\n,"a);
+else
+printf("%f\n,"c);
+}
+else
+{
+if(c.b)
+printf("%\n,"c);
+else
+printf("%f\n,"b);
+}
+}
