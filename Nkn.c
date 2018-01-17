e6b98dd06c1390a9055d64a640511293dd5c6e9f
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-17 06:55:29 -0800

    Create Nkn.c

---
+++ Nkn.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i;
+sum=0;
+scanf("%d%d."&n,&k);
+printf("%d%d,n,k);
+for(i=0;i<=n;i++)
+{
+printf("%d."a[i]);
+}
+for(i=1;i<=k;i++)
+{
+sum=sum+a[i];
+}
+ptintf("%d",sum);
+retrun 0;
+}
