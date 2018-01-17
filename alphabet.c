4568acab00f49a422395fd53f9036ae9a4320f98
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-17 06:19:35 -0800

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char alphabet;
+printf("enter an alphabet");
+alphabet=get char();
+if(is lower(alphabet));
+put char(to upper (alphabet));
+else
+put char(to lower(alphaber));
+}
