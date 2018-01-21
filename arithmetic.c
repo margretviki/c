2d7ea0bc3fb0fd8c3c55f772ac3aedf2d7823e12
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-21 06:54:22 +0000

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,13 @@
+#include<sttdio.h>
+void main()
+{
+int a=10,b=15,c=15;
+printf("arithmetic operators and returned values");
+printf("a+b="+{a+b));
+printf("b-a="+(b-a));
+printf("a*2="+(a*3));
+printf("b\5="=(b\5));
+printf("b%2="+(b52));
+printf("unary plus="++2);
+printf("unary minus="+-2);
+}
