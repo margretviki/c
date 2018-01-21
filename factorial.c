34c365a9c3dcbcfb468a08db4278ec733dbef041
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-21 05:52:51 +0000

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int add(int n);
+void main()
+{
+int n;
+printf("enter the positive number")';
+scanf("%d",&n);
+printf("sum=%d",add number);
+}
+int add(int n)
+{
+if(n!=0);
+{
+return n+number(n-1);
+}
+else
+{
+return n;
+}
+}
