c111fa262add251caefc9e76e432edf913161005
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-21 06:39:55 +0000

    Create string.c

---
+++ string.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[30];
+int i,flg=0,n=0;
+clrscr();
+printf("enter the string");
+scanf("%s",s);
+for(i=0;s[i]!='\0':i++)
+{
+if(s[i]=='0'||s[i]=='1'||s[i]=='3'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+}
+flag=flag+1;
+}
+}
+if(flag==0)
+{
+printf("given stringnot contain num")';
+else
+printf("given string contain number ");
+getch()
+}
