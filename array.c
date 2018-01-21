5b584182f29a9ae77d41ba833983c736c059216d
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-21 06:48:49 +0000

    Create array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int array[100],minimum,size,c,location=1;
+printf("enter the miminum of elements in array\n");
+scanf("%d",&size);
+printf("enter %d integers\n",size);
+for(c=0; c<size;c++)
+scanf("%d",&array[c]);
+minimum:array[0];
+for(c=1;c<size;c++)
+{
+if(array[c]<minimum)
+{
+min=array[c];
+location=c+1;
+}
+}
