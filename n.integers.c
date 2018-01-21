b96c8c2d033020abebe215ff19c326d99cc264e8
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-21 06:33:01 +0000

    Create n.integers.c

---
+++ n.integers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int[]array={10,-1,28,14,44,5,36,97,-18,11};
+printf("the content of the array are:");
+for(int i=0; i<10;i++)
+printf("|t|t|t array ["tit"]="+array[1]);
+}
+}
