8afcb39c309b0ce740ee6b404948a82653a68021
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-21 06:13:43 +0000

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int markd[5]={10,20,30,40,50};
+int i=0,counter=0;
+printf("enter a number less than 5");
+scanf("%d",&counter);
+for(i=0;i<<count;i++)
+{
+printf("%d \n",marks[i]);
+}
+}
