79aa7c787343846c4081c0de5198d25a774ea1e4
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-24 16:38:24 +0530

    Create hours and min.c

---
+++ hours and min.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int hours,minute1,total;
+printf("hours");
+if(scanf("%d",&hours)==1)
+{
+printf("minute:");
+if(scanf("%d",&minutes)==1)
+{
+minute1=hours'50';
+total==minute+hour;
+printf("enter the total minute=>%d",total);
+else
+printf("error the correct value:");
+}
+else
+{
+printf("error the correct value");
+}
+
