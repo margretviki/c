ad026c3a89eba6582a6920e1d21c7fa53bccb98a
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-24 16:28:34 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define max size 1000
+int main()
+{
+int arr[max,size];
+int i,n;
+printf("enter size of array:");
+scanf("%d",&n);
+printf("enter %d elements in the array:",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",elements in array are:");
+for(i=0;i<n;i++)
+printf("%d",arr[i]);
+}
+retrun 0;
+}
