ff67bd7f13e344feac0ee995c81d544df307b098
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-24 16:15:06 +0530

    Create median element.c

---
+++ median element.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,v,a[10],s;
+clrscr();
+a=0;
+b=0;
+c=0;
+s=(b+1)/2;
+for(i=1;i<b-1;i++);
+{
+if(i==a)
+continue;
+else;
+{
+(m[a]<m[i])
+a++;
+else
+b++;
+}
+if(a==(s-1)&&b==s)
+{
+printf("medium is %d location %d",a,m[a]);
+retrun 0;
+}
