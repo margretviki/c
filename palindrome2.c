df14cc4a69a15ce21c38b5f618b433e5113ba4dc
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-31 15:57:48 +0530

    Create palindrome2.c

---
+++ palindrome2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int mmain()
+{
+int rev,r,v,pal;
+printf("enter the number");
+scanf("%d",&n);
+rev=0;
+pal=n;
+while(n<0)
+{
+r=n%10;
+s=s*10+r;
+n=n/10;
+if(a==s)
+{
+printf("enter the number is palindrome");
+}
+else
+printf("enter the number is not a palindrome");
+}
+getch()
+}
