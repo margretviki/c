816b6c477147fc61b18d8dfc96b2fe145c82d15d
margretviki <35136002+margretviki@users.noreply.github.com>
2018-01-31 16:32:22 +0530

    Rename multiple.c to multiple2.c

---
+++ multiple2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a=13,b,c;
+printf("enter the value");
+scanf("%d",&b);
+if(a!0)
+{
+c=a+b;
+if(c>a)
+{
+printf("yes multiple %d",c);
+}
+else
+{
+printf("not multiple %d",c);
+}
+}
+return 0;
+}
