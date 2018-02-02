d226cd962f3ac54b8a87cc46e86924fb9b81321e
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-02 15:46:45 +0530

    Create swap2.c

---
+++ swap2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the values");
+	scanf("%d %d",&a,&b);
+	if(a!=10);
+	{
+	int t;
+	t=a;
+	a=b;
+	b=t;
+	printf("%d %d",a,b);
+	return 0;
+	}
+}
+	
+
+	
