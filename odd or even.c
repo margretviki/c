f0038b347876e7b71f240126735d46ecb79a7d46
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-05 15:37:45 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the value of a and b");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("the value of c is %d",c);
+if(c%2==0)
+{
+printf("the value is %d",c);
+}
+else
+{
+printf("the value is %d",c);
+}
+}
