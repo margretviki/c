2c45aac2ffa6fae5675f789fe2049f1ada65ce4e
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-05 15:55:03 +0530

    Create next number.c

---
+++ next number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+printf("enter any number");
+scanf("%d,&n");
+printf("natural number from 1 to %d:/n");
+for(i=1;i<=n;i++)
+{
+printf("%d /n,"i);
+}
+retrun 0;
+}
