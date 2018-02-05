7afb59f7f24a715bd24526b4b95ea80280ddf2d5
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-05 16:18:15 +0530

    Create count number.c

---
+++ count number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int char[i];
+string str="good girl";
+printf("enter the character:');
+scanf("%d",&char[i]);
+str=strlength(good girl);
+scanf("%s,&str);
+}
+return 0;
+}
