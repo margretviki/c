1a92c6600b5ba0d03f7518fab3e04b821db79cf3
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-05 16:51:46 +0530

    Create lowest andhighest.c

---
+++ lowest andhighest.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int n,a[1000],i,j,t;
+scanf("%d \n",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n-1;i++)
+{
+for(j=1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+printf("%d%d",a[0],a[n-1]);
+}
