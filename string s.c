08b63f4dc78b535a2cd330251e45619a425a627e
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-05 16:44:35 +0530

    Create string s.c

---
+++ string s.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+char a[50];
+printf("enter the value of n and a;);
+scanf("%s%d",a,n);
+printf("\n %s%d",a,n);
+for(i=1;i<=n;i++);
+{
+printf("%s",a);
+}
+return 0;
+}
