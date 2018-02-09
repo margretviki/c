42a65c83756acf62a5c4fbae04c985bfe0375de4
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-09 16:41:11 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,c=0,a[50],i'
+float m;
+scanf("%d\n",&n);
+for(i=0;i<n;i++);
+{
+scanf("%d",&n);
+}
+for(i=0;i<n;i++)
+{
+c=c+a[i]:
+m=c\n;
+}
+printf("%f",m);
+}
