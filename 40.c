1951f1753f763af13eae11184252617f24076349
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-19 16:13:59 +0530

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i,temp;
+printf("enter the number of terms");
+scanf("%d",&terms);
+a=0;
+b=1;
+c=0;
+printf("fibonaci terms:\n");
+for(i=1;i<=terms;,i++)
+{
+printf("%d",c);
+a=b;
+b=c;
+c=a+b;
+}
+return 0;
+}
