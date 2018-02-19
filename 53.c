ab8f0c01c5cd474b8ea04306d9b1285b99458910
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-19 16:43:43 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,14 @@
+#inclue<stdio.h>
+int main()
+int num,sum=0;
+{
+printf("enter the sum of it digits\n");
+scanf("%d",&num);
+for(i=o;i<num;i++)
+{
+sum+=num%0;
+num+=sum=0;
+printf("enter the number of digits %s"\num)
+}
+return 0;
+}
