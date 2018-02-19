6c1086e50588c0e18989d2d926fc034d1c6d930f
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-19 16:06:49 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char[1000];
+int a,count=1;
+printf("enter the number\n");
+scanf("%[^\n]s,",s);
+for(i=0;a[i]!='\0';i++)
+{
+if(s[i]==' ')
+return 0;
+}
+printf("enter the given number:%d\n",count+1);
+}
