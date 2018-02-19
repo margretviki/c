d1bb45b667d01e4cea1a00e04b62d013b5acfe4b
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-19 15:53:22 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,d;
+printf("enter the number");
+scanf("%d%d",&a,&b);
+d=a*b;
+if(d%2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+}
