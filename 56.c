04c273a8079acdd54990211780aa5d84faec947a
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-19 16:29:43 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,15 @@
+#inlcude<stdio.h>
+void main()
+{
+char c;
+printf("enter the alphabet number");
+for(i=a;i<<a;i++)
+{
+printf("if it is a alphabet number");
+}
+else
+{
+printf("if it is not a alphabet number");
+}
+return 0;
+}
