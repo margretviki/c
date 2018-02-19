548f0747a22fc4d97eb172b541cbe38f2ada4f76
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-19 15:37:38 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int n,num=0;
+printf("enter any number to print in words:");
+scanf("%d",&n);
+while(n!=0)
+{
+num=(num*10)+(n%10);
+n/=10;
+}
+while(num!=10)
+}
+{
+switch(num%10)
+{
+case0;
+printf("zero");
+break;
+case=1;
+printff("one");
+break;
+case=2;
+printf("two");
+break;
+cas3;
+printf("three");
+break;
+case=4;
+printf("four");
+break;
+case=5;
+printf("five");
+break;
+case=6;
+printf("six");
+break;
+case=7;
+printf("seven");
+break;
+case=9;
+printf("nine");
+break;
+return 0;
+}
