d32dcb9db8087204eb9eddeb43fbcf3e5f1aeefe
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-21 16:39:37 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,dec;
+int,char;
+printf("enter the decimal binary value");
+scanf("%d",&num);
+for(i=0;i<num;i++);
+{
+printf("the binary value is%d");
+}
+return 0;
+}
