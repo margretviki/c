f0cb4063a6ac245849d5a8310e00275764bf1bb4
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-26 16:23:05 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=2,b=4;
+char a,b,c;
+printf("enter the number");
+scanf("%d"&c);
+if((c>=a))&&((c<=b))
+{
+printf("yes");
+}
+else
+{
+printf("no');
+}
+}
