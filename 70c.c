7337c7641bff28a11222ad1f3be604e43b9cd052
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-26 15:53:25 +0530

    Create 70c.c

---
+++ 70c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#includeconio.h>
+void main()
+{
+int a,b;
+printf("enter the nearest value");
+scanf("%d",&a);
+printf("enter the two nearest value");
+scanf("%d",&b);
+printf("enter %d is,&a&b");
+}
