60a114bfcbc60f3e076f8c7ffa714b4f509bec90
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-26 15:46:58 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int exponent,base;
+int c;
+printf("enter the exponent");
+scanf("%d",&exponent);
+printf("enter the base");
+scanf("%d",&base");
+h=base^(exponent+1);
+printf("the result is %d",c);
+}
