9431c98433edfe7182a308ca50b6f1a9068b0a0e
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-28 15:43:31 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ float n = 1.5;
+int main;
+{
+      float b;
+
+       printf("Please enter a value to be rounded: ");
+       scanf("%f", &n);
+
+       n = n + add;
+       n = (int)n;
+
+       printf("The rounded value is %0.8", n);
+
+      return 0;
+}
