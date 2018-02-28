70ec2267642212a339244c52fde1ee739444655c
margretviki <35136002+margretviki@users.noreply.github.com>
2018-02-28 16:14:20 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m,v;
+printf("enter the number");
+scanf("%d",&n);
+ for(m=0;m<=n;m++)
+ {
+   if(v=m*m)
+{
+printf("its a perfect square");
+}
+else 
+{
+printf("its is not a prefect square");
+}
+}
+}
