d3d617be5a3434fc3e7a09ce855efb8e55c544ba
margretviki <35136002+margretviki@users.noreply.github.com>
2018-03-14 16:18:48 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  float l,b,Area;
+  printf("Enter the length:");
+  scanf("%f",&l);
+  {
+  printf("Enter he breadth:");
+  scanf("%f",&b);
+  {
+  Area=l*b;
+  printf("Area of farm is: %.5f",Area);
+}
+}
+}
