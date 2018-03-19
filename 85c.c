6ca02e0b145ce9de7c480fadcab714978b8f1d41
margretviki <35136002+margretviki@users.noreply.github.com>
2018-03-19 15:50:14 +0530

    Rename 85.c to 85c.c

---
+++ 85c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	char a[60]="XCODE";
+	int i;
+	for(i=0;i<=6;i++)
+	{
+	if(i%2==0)
+	{
+	printf("%c",a[i]);
+	}
+	printf(" ");
+	}
+	for(i=1;i<=6;i++)
+	{
+	if(i%2!=0)
+		{
+		printf("%c",a[i]);
+		}
+		return 0;
+}
+}
+
+
